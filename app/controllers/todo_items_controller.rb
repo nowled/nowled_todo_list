ed9001c1296a25536f641b388452191744deafd4
chaz <nowledster@gmail.com>
2017-09-22 08:50:37 +0000

    Finished Everything Before Stylings

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,6 +1,7 @@
 class TodoItemsController < ApplicationController
     
      before_action :set_todo_list
+     before_action :set_todo_item, except: [:create]
      
     
     def create
@@ -9,7 +10,6 @@ class TodoItemsController < ApplicationController
     end
     
     def destroy
-        @todo_item = @todo_list.todo_items.find(params[:id])
         if @todo_item.destroy
             flash[:success] = "Todo List Item Was Fucking Deleted"
         else
@@ -17,9 +17,19 @@ class TodoItemsController < ApplicationController
         end 
         redirect_to @todo_list
     end 
+    def complete
+        @todo_item.update_attribute(:completed_at, Time.now)
+        redirect_to @todo_list, notice: "Todo Item Completed"
+    end 
+    
+    private
     def set_todo_list
      @todo_list = TodoList.find(params[:todo_list_id])
     end
+    
+    def set_todo_item
+        @todo_item = @todo_list.todo_items.find(params[:id])
+    end 
     
     def todo_item_params
         
